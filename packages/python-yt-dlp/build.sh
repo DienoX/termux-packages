17506ab43ac1fc06d563861334cf3c5fb14c959f
Yaksh Bariya <thunder-coding@termux.dev>
2026-01-24 02:06:01 +0530

    rebuild(main/python-yt-dlp): with python 3.13

--- packages/python-yt-dlp/build.sh
+++ packages/python-yt-dlp/build.sh
@@ -3,6 +3,7 @@ TERMUX_PKG_DESCRIPTION="A youtube-dl fork with additional features and fixes"
 TERMUX_PKG_LICENSE="Unlicense"
 TERMUX_PKG_MAINTAINER="Joshua Kahn @TomJo2000"
 TERMUX_PKG_VERSION="2026.01.31"
+TERMUX_PKG_REVISION=1
 TERMUX_PKG_SRCURL=https://github.com/yt-dlp/yt-dlp/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz
 TERMUX_PKG_SHA256=8a9a82ab5a222fb88ace30d94521150b03c66910b3a5830a3f1b32fcc5a94893
 TERMUX_PKG_DEPENDS="libc++, libexpat, openssl, python, python-brotli, python-pip, python-pycryptodomex"
