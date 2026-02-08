a2db75c5be227c1697683669fd5ebaef4b0107ff
DienoX <matrix19994story@gmail.com>
2026-02-08 13:17:54 +0100

    Update build.sh

--- x11-packages/scrcpy/build.sh
+++ x11-packages/scrcpy/build.sh
@@ -1,30 +1,18 @@
 TERMUX_PKG_HOMEPAGE=https://github.com/Genymobile/scrcpy
-TERMUX_PKG_DESCRIPTION="Scrcpy Dev (SDL3 Version)"
+TERMUX_PKG_DESCRIPTION="Scrcpy Dev (SDL3)"
 TERMUX_PKG_LICENSE="Apache-2.0"
 TERMUX_PKG_MAINTAINER="@twoj_nick"
-
-# Wersja deweloperska - nadpisujemy, aby była 'najnowsza'
-TERMUX_PKG_VERSION="3.4.0-dev-sdl3"
-
-# Pobieramy kod z brancha master (który teraz używa SDL3)
+TERMUX_PKG_VERSION="3.4.0-dev"
 TERMUX_PKG_SRCURL=https://github.com/Genymobile/scrcpy/archive/master.tar.gz
 TERMUX_PKG_SHA256="SKIP_CHECKSUM"
-
 TERMUX_PKG_AUTO_UPDATE=false
 
-# KLUCZOWA ZMIANA: Zmieniamy sdl2 na libsdl3
-# Usuwamy też sdl2-compat
-TERMUX_PKG_DEPENDS="android-tools, ffmpeg, libusb, libsdl3"
+# TU BYŁ BŁĄD. Teraz jest poprawnie:
+TERMUX_PKG_DEPENDS="android-tools, ffmpeg, libusb, sdl3"
 
-# Usuwamy stare zabezpieczenia przed sdl2
-TERMUX_PKG_ANTI_BUILD_DEPENDS="android-tools"
+TERMUX_PKG_ANTI_BUILD_DEPENDS="android-tools, sdl2, sdl2-compat"
 
-# Serwer: Nadal musimy "pożyczyć" najnowszy stabilny serwer, 
-# chyba że masz własny plik .jar zbudowany ręcznie.
-# UWAGA: Jeśli protokół w dev (SDL3) zmienił się drastycznie, 
-# stary serwer v2.4/v3.x może nie zadziałać poprawnie.
 LATEST_STABLE_SERVER="3.3.4"
-
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -Dprebuilt_server=$TERMUX_PKG_SRCDIR/scrcpy-server-v${LATEST_STABLE_SERVER}
 -Dcompile_server=false
@@ -32,10 +20,6 @@ TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 "
 
 termux_step_post_get_source() {
-    # Pobieranie serwera (workaround dla braku buildera Java w Termux Docker)
-    local _server_ver="${LATEST_STABLE_SERVER}"
-    local _url="https://github.com/Genymobile/scrcpy/releases/download/v${_server_ver}/scrcpy-server-v${_server_ver}"
-    
-    echo "Pobieranie serwera v${_server_ver}..."
+    local _url="https://github.com/Genymobile/scrcpy/releases/download/v${LATEST_STABLE_SERVER}/scrcpy-server-v${LATEST_STABLE_SERVER}"
     termux_download ${_url} $(basename ${_url}) SKIP_CHECKSUM
 }
